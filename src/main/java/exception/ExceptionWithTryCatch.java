5239b1660d974bab852bbb11d5042ce0aa7fa8fc
bfhuang <bfhuang@thoughtworks.com>
2013-12-15 21:36:06 +0800

    types of printing out the exception message

--- src/main/java/exception/ExceptionWithTryCatch.java
+++ src/main/java/exception/ExceptionWithTryCatch.java
@@ -1,6 +1,18 @@
 package exception;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.logging.Logger;
+
 public class ExceptionWithTryCatch {
+
+	private static Logger logger =
+			Logger.getLogger("LoggingExceptions2");
+	static void logException(Exception e) {
+		StringWriter trace = new StringWriter();
+		e.printStackTrace(new PrintWriter(trace));
+		logger.severe(trace.toString());
+	}
 
 	public static void main(String[] args) {
 		int sign = 1;
@@ -8,9 +20,12 @@ public class ExceptionWithTryCatch {
 		System.out.println("Before exception");
 		if(1 == sign){
 			try {
-				throw new NoSuchFieldException();
+				throw new NoSuchFieldException("in main");
 			} catch (NoSuchFieldException e) {
-				e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+				e.printStackTrace();
+				e.printStackTrace(System.out);
+				e.printStackTrace(new PrintWriter(new StringWriter()));
+				logException(e);
 			}
 		}
 
